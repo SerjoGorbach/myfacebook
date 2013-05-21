0f5d92f34e66830fa10fe24cf07d9410c5206448
SerjoGorbach <Kremi@ukr.net>
2013-05-21 16:09:09 +0300

    21

---
+++ gorila.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include "Sempanzr.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  cout<<fixed<<setprecision(0);
+	double sum=0;
+	its my;
+	my.trymy(50000000);
+	_getch();
+	return 0;
+}
